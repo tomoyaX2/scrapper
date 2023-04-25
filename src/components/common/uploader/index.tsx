0d57ef79dc9be493ad118b5d421f34f491983e41
tomoya <kalinichenko1999@gmail.com>
2023-04-25 10:15:20 +0300

    add loader to uploader

--- src/components/common/uploader/index.tsx
+++ src/components/common/uploader/index.tsx
@@ -1,4 +1,5 @@
-import { Uploader } from 'rsuite';
+import { useState } from 'react';
+import { Loader, Uploader } from 'rsuite';
 import { useAppDispatch, useAppSelector } from 'src/store';
 import { Image } from 'src/components/common/image';
 import { getUser } from 'src/store/user';
@@ -6,17 +7,25 @@ import { getUser } from 'src/store/user';
 const UploadAvatar = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(state => state.user.data);
+  const [isLoading, setLoading] = useState(false);
+  console.log(isLoading, '1231');
   return (
     <Uploader
       fileListVisible={false}
       listType='picture'
       action='https://mangamischief.com/backend/users/upload-avatar'
       headers={{ access_token: localStorage.getItem('accessToken') }}
+      onChange={() => {
+        setLoading(true);
+      }}
       onSuccess={() => {
+        setLoading(false);
         dispatch(getUser());
       }}
     >
       <button style={{ width: 300, height: 300 }}>
+        {isLoading && <Loader backdrop center className='z-10' />}
+
         {user?.avatarUrl && (
           <Image url={user.avatarUrl} width={300} height={300} alt='' />
         )}
