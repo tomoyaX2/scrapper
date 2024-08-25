f095b0826f572788dc5acfe6b858bd40184519ed
tomoyaX2 <alexk@cerebrate.ai>
2024-08-25 13:56:52 +0300

    show container

--- src/routes/container-widget-testing/container-widget-testing.tsx
+++ src/routes/container-widget-testing/container-widget-testing.tsx
@@ -55,7 +55,7 @@ const WidgetTesting = (): JSX.Element => {
         //@ts-expect-error
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         window.MdBot.init({
-          type: 'popup',
+          type: 'container',
           apiKey: 'streaming_web-yakov-karda-gmail-com'
         });
       }
