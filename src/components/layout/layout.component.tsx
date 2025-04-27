dc95449d659e98a127e747b08014b71b4106a203
tomoyaX2 <alexk@cerebrate.ai>
2025-04-27 16:58:48 +0300

    projectId

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -13,11 +13,11 @@ const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
         strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "YOUR_PROJECT_ID");
+     (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "raq720d9hb");
           `
         }}
       />
